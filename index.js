0f520a88250eb60e41b6d4e8f5e6ef525e5e480e
rajxode <rajsh1202@gmail.com>
2023-09-08 01:04:54 +0530

    add two ejs pages for get and post method along with their routes and controllers

--- index.js
+++ index.js
@@ -6,11 +6,35 @@ const {PORT} = process.env;
 
 const app = express();
 
+const fileUpload = require('express-fileupload');
+
+app.set('view engine' ,'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/',
+}))
 
 app.get('/',(req,res) => {
-    res.send('welcome');
+    res.send('Welcome to file upload project');
+})
+
+app.get('/getForm',(req,res) => {
+    res.render('getForm');
+})
+
+app.get('/postForm',(req,res) => {
+    res.render('postForm');
+})
+
+app.get('/getFormData', (req,res) => {
+    res.send(req.query);
+})
+
+app.post('/postFormData', (req,res) => {
+    console.log(req.files);
+    res.send(req.body);
 })
 
 app.listen(PORT,() => console.log(`server is running on port: ${PORT}`));
