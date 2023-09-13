4f4b6b2e004d5d688b1aac522865a915fa45b4f0
rajxode <rajsh1202@gmail.com>
2023-09-14 00:44:19 +0530

    add cloudinary config and upload images on cloudinary (single and multiple)

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 require('dotenv').config();
 const express = require('express');
 
+const cloudinary = require('cloudinary').v2;
+
 const {PORT} = process.env;
 
 const app = express();
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDNAME,
+    api_key:process.env.CLOUD_API_KEY,
+    api_secret:process.env.CLOUD_API_SECRET
+})
 
 const fileUpload = require('express-fileupload');
 
@@ -28,13 +36,42 @@ app.get('/postForm',(req,res) => {
     res.render('postForm');
 })
 
+app.get('/postMultiForm',(req,res) => {
+    res.render('postMultiImages');
+})
+
 app.get('/getFormData', (req,res) => {
     res.send(req.query);
 })
 
-app.post('/postFormData', (req,res) => {
-    console.log(req.files);
+app.post('/postFormData', async (req,res) => {
+    let file = req.files.sampleFile;
+    const result = await cloudinary.uploader.upload(file.tempFilePath,{
+        folder:process.env.FOLDER
+    });
     res.send(req.body);
+})
+
+
+app.post('/postMultiImage', async (req,res) => {
+    let result;
+    let imageArray = [];
+
+    if(req.files){
+        for(let i = 0; i < req.files.sampleFile.length; i++){
+            result = await cloudinary.uploader.upload(req.files.sampleFile[i].tempFilePath, {
+                folder: process.env.FOLDER
+            })
+
+            imageArray.push({
+                public_id:(await result).public_id,
+                secure_url: (await result).secure_url
+            })
+        }
+    }
+
+    console.log(imageArray);
+    res.send('file uploaded');
 })
 
 app.listen(PORT,() => console.log(`server is running on port: ${PORT}`));
